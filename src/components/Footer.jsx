0d82311d0b0c92ba7156a548f4884da504e3a071
AlexArgese <101403816+AlexArgese@users.noreply.github.com>
2025-06-08 22:14:18 +0200

    footer AI image sentence

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,32 @@ import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-container1">
-        <img src="/logo_esteso_trasp.png" className='logo-footer'/>
-        Copyright © 2025 | EURECOM | All rights reserved.
-      </div>
-      <div className="footer-container2">
-        <div className='line-footer'>
-          <img src="/Al4Health.png" alt="Al4Health@EURECOM"/>
-          <span className='partOf'>part of Al4Health@EURECOM</span> 
+    <footer className='big-footer'>
+      <div className="footer">
+        <div className="footer-container1">
+          <img src="/logo_esteso_trasp.png" className='logo-footer'/>
+          Copyright © 2025 | EURECOM | All rights reserved.
         </div>
-        <div className='line-footer'>
-          <img src="/logo_alex.png" alt="Al4Health@EURECOM"/>
-          <span className='partOf'>Website, graphics & animations by <strong>
-            <a target="_blank"
-            rel="noopener noreferrer"
-            style={{color:'#ff4081'}}
-            href='https://alexargese.github.io'>Alex Argese
-            </a>
-          </strong></span> 
+        <div className="footer-container2">
+          <div className='line-footer'>
+            <img src="/Al4Health.png" alt="Al4Health@EURECOM"/>
+            <span className='partOf'>part of Al4Health@EURECOM</span> 
+          </div>
+          <div className='line-footer'>
+            <img src="/logo_alex.png" alt="Al4Health@EURECOM"/>
+            <span className='partOf'>Website, graphics & animations by <strong>
+              <a target="_blank"
+              rel="noopener noreferrer"
+              style={{color:'#ff4081'}}
+              href='https://alexargese.github.io'>Alex Argese
+              </a>
+            </strong></span> 
+          </div>
         </div>
       </div>
+      <p className="footer-note">
+        Some images on this website are AI-generated.
+      </p>
     </footer>
   )
 }
